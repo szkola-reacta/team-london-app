dae4c9bb969c04a26288dc3c81efb69e3deab536
Piotrek Teresinski <piotrek@dzem.com.pl>
2021-04-17 12:54:34 +0200

    add AddToCart component, use it in ProductBox component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { ProductBox } from './components/ProductBox';
 function App() {
   return (
     <div className="App">
-      {/* <ProductBox
+      <ProductBox
         id={1}
         image="media/products/joust-bag.jpeg"
         name="Joust Duffle Bag"
         price="34€"
-      /> */}
+      />
     </div>
   );
 }
