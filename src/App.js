e298a824490ea4d04a6c2ede77903a253aa5bf71
PabloMD <chmielecki7@gmail.com>
2021-04-17 12:44:31 +0200

    TL-29 Strona produktu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductDetails from "./pages/ProductDetails";
+
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+import { Box, Button, Center, Container, Flex, Heading, HStack, Image, Spacer, Square, Tab, TabList, TabPanel, TabPanels, Tabs, Text, VStack } from "@chakra-ui/react";
+
+function ProductDetails() {
+  const params = useParams();
+  const [product, setProduct] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetch("/data/products.json")
+      .then((response) => response.json())
+      .then((data) => {
+          const p = data.find((p) => p.id === parseInt(params.productId));
+          p.rating = Math.floor(Math.random() * (5));
+          setProduct(p);
+      })
+      .catch((error) => setError(error));
+
+    return () => {};
+  }, [params.productId]);
+
+  if(error){
+      return(<div>Not such product ({error})</div>);
+  }
+  if(!product){
+      return(<div>Not such product</div>);
+  }
+
+  return (
+    <Container maxW="container.xl">
+      <Flex align="center" justify="center" spacing="24px">
+        <Center>
+          <Box boxSize="sm">
+            <Image src={`/${product.image}`} alt={product.name} />
+          </Box>
+        </Center>
+        <Box>
+          <Heading mb={4}>{product.name}</Heading>
+          <Box>{`<RatingsComponent starts={${product.rating}}`}</Box>
+
+          <Flex justify="space-between" spacing="24px">
+            <VStack align="left">
+              <Box>
+                <Text>As low as</Text>
+              </Box>
+              <Box>
+                <Text align="left">
+                  €{parseFloat(product.price).toFixed(2, 10)}
+                </Text>
+              </Box>
+            </VStack>
+            <VStack align="right">
+              <Text>IN STOCK</Text>
+              <Text>SKU# PROD_{product.id}</Text>
+            </VStack>
+          </Flex>
+
+          <Button colorScheme="blue">Add to Cart Comp (todo)</Button>
+        </Box>
+      </Flex>
+
+      <Tabs>
+        <TabList>
+          <Tab>Details</Tab>
+          <Tab>More Info</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>{product.name}</p>
+          </TabPanel>
+          <TabPanel>
+            <p>{product.description}</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      <VStack>
+        <heading>We found other products you might like!</heading>
+        <Box>Related products component</Box>
+      </VStack>
+    </Container>
+  );
+}
+
+export default ProductDetails;
