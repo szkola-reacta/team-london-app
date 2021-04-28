43e0ed5e71c005a73f924703912f27ba92fc7910
Maja Wrońska <mwrzosek@e-point.pl>
2021-04-28 07:23:08 +0200

    TL-11 Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProductDetails from "./pages/ProductDetails";
 
 import MainMenu from "./components/MainMenu";
 import { Header } from "./components/Layout";
+import { Footer } from "./components/Layout";
 
 function App() {
   return (
@@ -16,13 +17,15 @@ function App() {
           <Route path="/product/:productId" component={ProductDetails} />
           <Route path="/search/:searchTerm">We will find something!:)</Route>
         </Switch>
+        <ProductBox
+          id={1}
+          image="media/products/joust-bag.jpeg"
+          name="Joust Duffle Bag"
+          price="34€"
+        />
+        <Footer />
       </Router>
-      <ProductBox
-        id={1}
-        image="media/products/joust-bag.jpeg"
-        name="Joust Duffle Bag"
-        price="34€"
-      />
+
     </div>
   );
 }
